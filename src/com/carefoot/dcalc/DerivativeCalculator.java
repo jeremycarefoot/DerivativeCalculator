9a33282ebe30a9e2d2f22e9cc412ee5a33b3760a
Jeremy Carefoot <jeremycarefoot@gmail.com>
2024-01-12 12:36:02 -0700

    first commit

---
+++ src/com/carefoot/dcalc/DerivativeCalculator.java
@@ -0,0 +1,23 @@
+/*
+ * Derivative Calculator
+ * Version 1.0 - January 2024
+ * Developed by Jeremy Carefoot
+ * 
+ * Takes a mathematical function and attempts to find the derivative using basic differentiation rules
+ * 
+ */
+
+package com.carefoot.dcalc;
+
+public class DerivativeCalculator {
+	
+	// Main method
+	public static void main(String[] args) {
+		
+	}
+	
+	private static void setup() {
+		
+	}
+
+}
